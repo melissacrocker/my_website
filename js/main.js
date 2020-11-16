21db2ae67b7a931c871b59edaf894a3487842091
melissacrocker <49253582+melissacrocker@users.noreply.github.com>
2020-11-15 20:22:05 -0700

    Clean up code

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
 /* js by Melissa Crocker, 2020 */
+//First line of main.js...wrap everything in a self-executing anonymous function to move to local scope
+(function(){
+
+//pseudo-global variables for data join
+var attrArray = ["totCases", "totDeaths", "prctCaseDie", "prctUnins", "prctPOV"]; //list of attributes
+var expressed = attrArray[0]; //initial attribute
 
 //begin script when window loads
 window.onload = setMap();
@@ -39,35 +45,8 @@ function setMap(){
     function callback(error, csvData, us, colorado){
         //translate us Topojson
         var usStates = topojson.feature(us, us.objects.US_State_Boundaries),
-        
-        //translate colorado TopoJSON
+            //translate colorado TopoJSON
             coloradoCounties = topojson.feature(colorado, colorado.objects.Colorado_County_Boundaries).features;
-        
-        //variables for data join
-        var attrArray = ["totCases", "totDeaths", "prctCaseDie", "prctUnins", "prctPOV"];
-
-        //loop through csv to assign each set of csv attribute values to geojson county
-        for (var i=0; i<csvData.length; i++){
-            var csvCounty = csvData[i]; //the current county
-            var csvKey = csvCounty.countyFIPS; //the CSV primary key
-
-            //loop through geojson counties to find correct county
-            for (var a=0; a<coloradoCounties.length; a++){
-
-                var geojsonProps = coloradoCounties[a].properties; //the current region geojson properties
-                var geojsonKey = geojsonProps.countyFIPS; //the geojson primary key
-
-                //where primary keys match, transfer csv data to geojson properties object
-                if (geojsonKey == csvKey){
-
-                    //assign all attributes and values
-                    attrArray.forEach(function(attr){
-                        var val = parseFloat(csvCounty[attr]); //get csv attribute value
-                        geojsonProps[attr] = val; //assign attribute and value to geojson properties
-                    });
-                };
-            };
-        };
             
         //add US states to map
         var states = map.append("path")
@@ -75,16 +54,53 @@ function setMap(){
             .attr("class", "states")
             .attr("d", path);
         
-        //add Colorado counties to map
-        var counties = map.selectAll(".counties")
-            .data(coloradoCounties)
-            .enter() //create elements
-            .append("path") //append elements to svg
-            .attr("class", function(d){
-                return "counties " + d.properties.countyFIPS;
-            })
-            .attr("d", path); //project data as geometry in svg
-        //console.log(csvData);
+        //join csv data to GeoJSON enumeration units
+        coloradoCounties = joinData(coloradoCounties, csvData);
+        
+        //add enumeration units to the map
+        setEnumerationUnits(coloradoCounties, map, path);
+        
         console.log(coloradoCounties);
     };
+};//end of setMap()
+    
+function joinData(coloradoCounties, csvData){
+    //loop through csv to assign each set of csv attribute values to geojson county
+    for (var i=0; i<csvData.length; i++){
+        var csvCounty = csvData[i]; //the current county
+        var csvKey = csvCounty.countyFIPS; //the CSV primary key
+
+        //loop through geojson counties to find correct county
+        for (var a=0; a<coloradoCounties.length; a++){
+
+            var geojsonProps = coloradoCounties[a].properties; //the current region geojson properties
+            var geojsonKey = geojsonProps.countyFIPS; //the geojson primary key
+
+            //where primary keys match, transfer csv data to geojson properties object
+            if (geojsonKey == csvKey){
+
+                //assign all attributes and values
+                attrArray.forEach(function(attr){
+                    var val = parseFloat(csvCounty[attr]); //get csv attribute value
+                    geojsonProps[attr] = val; //assign attribute and value to geojson properties
+                });
+            };
+        };
+    };
+
+    return coloradoCounties;
 };
+
+function setEnumerationUnits(coloradoCounties, map, path){
+    //add Colorado counties to map
+    var counties = map.selectAll(".counties")
+        .data(coloradoCounties)
+        .enter() //create elements
+        .append("path") //append elements to svg
+        .attr("class", function(d){
+            return "counties " + d.properties.countyFIPS;
+        })
+        .attr("d", path); //project data as geometry in svg
+};
+    
+})(); //last line of main.js
